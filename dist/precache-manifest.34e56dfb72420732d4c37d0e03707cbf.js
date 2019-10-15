54f1a1ab829dacd7dfc053eb8e23674de6585fcf
Juarez Lustosa <juarez.lustosa@gmail.com>
2019-10-15 00:30:59 -0300

    Configs for Heroku server

---
+++ dist/precache-manifest.34e56dfb72420732d4c37d0e03707cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f474294215101fa8f296",
+    "url": "/css/app.0200d607.css"
+  },
+  {
+    "revision": "f474294215101fa8f296",
+    "url": "/js/app.9d17a77c.js"
+  },
+  {
+    "revision": "00e470d8e9fd106f1efe",
+    "url": "/js/chunk-vendors.bcbdbe80.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d01509163120bb9828cd1792a3fea053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
