83e996af70ced5517a9631b27f9a8aaf2eb4a2f0
Arabi Alhumsi <araby.ami@gmail.com>
2019-10-16 02:05:32 +0200

    Added a simple list to the frontend, and enabled cors in backend

---
+++ src/components/ListPullRequests.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-for="user in users" v-bind:key="user.username">
+      <h3>{{user.username}}</h3>
+      <ul v-for="(pr, index) in user.pullRequests" v-bind:key="index">
+        <li>{{pr}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from '../api'
+
+export default {
+  name: 'ListPullRequests',
+  data: () => ({
+    users: []
+  }),
+  async mounted () {
+    try {
+      let result = await api.getPulls(2019)
+      this.users = result.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
