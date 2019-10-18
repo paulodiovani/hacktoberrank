ecd1f7c95da04d397b2a0fc41152293405f74fdd
Arabi Alhumsi <araby.ami@gmail.com>
2019-10-18 23:40:36 +0200

    Serve front-end dist from express app

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./api/router')
+const path = require('path')
+
+require('dotenv').config();
 
 const app = express()
 
+app.use('/', express.static(path.join(__dirname, '../dist')))
+
 app.use('/api/v1', cors(), router)
 
-const port = 8001
+const port = process.env.DEFAULT_BACKEND_PORT
 
 app.listen(port, () =>
   console.log(`backend service listening at port ${port}`)
