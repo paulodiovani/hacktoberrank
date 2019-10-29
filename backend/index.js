70220d0b0525724c92399dcba8288dadd1808291
Hiroshi Haji <hirohaji@users.noreply.github.com>
2019-10-29 15:54:38 -0400

    changing status log to debug on server init

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./api/router')
+const debug = require('debug')('index')
 
 const app = express()
 
@@ -9,5 +10,5 @@ app.use('/api/v1', cors(), router)
 const port = 8001
 
 app.listen(port, () =>
-  console.log(`backend service listening at port ${port}`)
+  debug(`backend service listening at port ${port}`)
 )
