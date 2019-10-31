db3eaf4b82a2a11621db231ec856acbcf6e34a5e
Kamal Eddin <kamalalhomsi2015@gmail.com>
2019-10-31 18:53:01 +0200

    feat: design suggestions

--- src/components/ListPullRequests.vue
+++ src/components/ListPullRequests.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="list-pull-request">
-    <div class="pr-title">Pull Requests List</div>
+    <div class="pr-title">Hacktober Fest 2019</div>
     <div class="pr-list">
       <div
         v-for="(user, userIndex) in users"
         v-bind:key="user.username"
         class="pr-item"
       >
-        <ul
-          v-for="(pr, index) in user.pullRequests"
-          v-bind:key="index"
-          class="pr-content"
-        >
-          <li class="pr-li">
-            <template v-if="userIndex < 3">
-              <img
-                class="medal"
-                :src="require('@/assets/' + svgMedal(userIndex))"
-              />
-            </template>
-            <div class="hover-effect">#{{ userIndex + 1 }}</div>
-            <span class="pr-rank">#{{ userIndex + 1 }}</span>
-            <div class="pr-username">{{ user.username }}</div>
+        <template v-if="userIndex < 3">
+          <img
+            class="medal"
+            :src="require('@/assets/' + svgMedal(userIndex))"
+          />
+        </template>
+        <div class="pr-rank-outter">
+          <div class="hover-effect">#{{ userIndex + 1 }}</div>
+          <span class="pr-rank">#{{ userIndex + 1 }}</span>
+        </div>
+        <div class="pr-username">{{ user.username }}</div>
+        <ul class="pr-links">
+          <li
+            v-for="(pr, index) in user.pullRequests"
+            v-bind:key="index"
+            class="pr-li"
+          >
             <a :href="pr" class="pr-link">{{ pr | cutPRLink }}</a>
           </li>
         </ul>
@@ -44,7 +46,7 @@ export default {
       this.users = result.data
     } catch (error) {
       // eslint-disable-next-line
-      console.log(error) 
+      console.log(error)
     }
   },
   methods: {
@@ -77,7 +79,7 @@ export default {
   right: 0;
   width: 40px;
   height: 40px;
-  top: -20px;
+  top: 0px;
   z-index: 9999999999;
   display: block;
 }
@@ -95,6 +97,11 @@ export default {
   font-size: 30px;
   transform: translate(-120px, 0px);
   transition: 0.3s;
+}
+
+.pr-rank-outter {
+  display: flex;
+  align-items: center;
 }
 
 .pr-title {
@@ -133,13 +140,7 @@ export default {
   color: #e6009a;
   font-weight: bold;
   font-size: 17px;
-}
-
-.pr-li {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
+  width: 30%;
 }
 
 .pr-rank {
@@ -147,14 +148,18 @@ export default {
   color: #c0c0c0;
 }
 
+.pr-links {
+  width: 50%;
+}
+
+.pr-li {
+  text-align: left;
+}
+
 .pr-link {
   text-decoration: none;
   color: #fff;
-  padding: 0px;
-  padding: 5px 14px;
-  width: 30%;
   font-size: 14px;
-  word-break: break-word;
   transition: 0.3s;
   &:hover {
     color: #5392fb;
@@ -174,6 +179,10 @@ export default {
   .pr-list {
     margin-top: 30px;
     .pr-item {
+      display: flex;
+      justify-content: space-between;
+      padding: 20px 30px;
+      align-items: center;
       width: 50%;
       background-color: #1d2c4e;
       text-overflow: ellipsis;
@@ -184,7 +193,7 @@ export default {
         transform: scale(1.04, 1.04);
         margin: 30px auto;
         .hover-effect {
-          transform: translate(-10px, 0px);
+          transform: translate(-1px, 0px);
         }
       }
     }
