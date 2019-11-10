7fe73a753631aadabadf34cc16540fcc15ea70f6
arabialhumsi <araby.ami@gmail.com>
2019-11-10 22:25:22 +0100

    add host env variable

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 const port = process.env.VUE_APP_DEFAULT_BACKEND_PORT || 8001
+const host = process.env.VUE_APP_DEFAULT_BACKEND_HOST || 'http://localhost'
 
 const api = {
   getPulls (year) {
-    return axios.get(`http://localhost:${port}/api/v1/pulls/${year}`)
+    return axios.get(`${host}:${port}/api/v1/pulls/${year}`)
   }
 }
 
